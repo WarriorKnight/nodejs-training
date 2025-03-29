19c58a1c62c55c41f7450e358c4374f480f15658
Vojtěch Škopek <39000132+WarriorKnight@users.noreply.github.com>
2025-03-29 21:50:24 +0100

    Added comments

--- 2. CLI tool/svatky/index.js
+++ 2. CLI tool/svatky/index.js
@@ -2,7 +2,7 @@
 
 /**
  * svatky
- * .
+ * CLI tool to check name days in the Czech Republic.
  *
  * @author darkk <..>
  */
@@ -19,6 +19,7 @@ const FgRed = "\x1b[31m";
 	await init({ clear });
 	debug && log(flags);
 
+	// Show help message if the user includes 'help' in input
 	if (input.includes(`help`)) {
 		console.log(
 		"Example Usage:\n",
@@ -27,12 +28,16 @@ const FgRed = "\x1b[31m";
 			return;
 		}
 
-
+	// Get the name from the CLI input
 	const name = input[0];
+
+	//Check if name was provided
 	if (!name){
 		console.log(FgRed + "Jméno nebylo načteno.");
 	}
+
 	else{
+		//Make an API request to retrieve the name day for the name
 		const response = await axios.get('https://svatky.adresa.info/json?name=' + name);
 		if (Array.isArray(response.data) && response.data.length!=0){
 			const date = response.data[0].date;
@@ -40,6 +45,8 @@ const FgRed = "\x1b[31m";
 			const month = date.slice(2,4);
 			console.log(name, "má svátek",FgRed + day + "." + month);
 		}
+
+		//Name wasn't found
 		else{
 			console.log("Zkontrolujte zadané jméno.");
 		}
