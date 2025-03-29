74c8b3949890021bc7e25c173c48278327839a71
Vojtěch Škopek <39000132+WarriorKnight@users.noreply.github.com>
2025-03-29 21:56:24 +0100

    Implement error handling

--- 3. Web Sraper/index.js
+++ 3. Web Sraper/index.js
@@ -1,30 +1,29 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
-import express from 'express'
 
+(async () => {
+    try {
+        // Fetch the website
+        const res = await axios('https://webscraper.io/test-sites/e-commerce/allinone');
+        const htmlData = res.data;
+        const $ = cheerio.load(htmlData);
+        let totalPrice = 0;
+        let productCount = 0;
 
-const PORT = 3000;
-const app = express();
+        // Scrape the prices and calculate some statistics
+        $('.price', htmlData).each((index, element) => {
+            const priceText = $(element).text();
+            const price = parseFloat(priceText.match(/\d+/g).join(''));
+            totalPrice += price;
+            productCount += 1;
+        });
 
-
-axios('https://webscraper.io/test-sites/e-commerce/allinone')
-.then(res => {
-    const htmlData = res.data;
-    const $ = cheerio.load(htmlData);
-    let totalPrice = 0;
-    let productCount = 0;
-
-    $('.price', htmlData).each((index, element) => {
-        const priceText = $(element).text();
-        const price = parseFloat(priceText.match(/\d+/g).join(''));
-        totalPrice += price;
-        productCount += 1;
-    });
-
-    const averagePrice = productCount > 0 ? (totalPrice / productCount).toFixed(2) : 0;
-    console.log("Total price is:", totalPrice);
-    console.log("Total products:", productCount);
-    console.log("Average price is:", averagePrice);
-});
-
-app.listen(PORT, () => console.log("Server is listening on 3000"));
+        const averagePrice = productCount > 0 ? (totalPrice / productCount).toFixed(2) : 0;
+        console.log("Total price is:", totalPrice);
+        console.log("Total products:", productCount);
+        console.log("Average price is:", averagePrice);
+    } catch (error) {
+        // Handle errors
+        console.error("An error occurred:", error.message);
+    }
+})();
